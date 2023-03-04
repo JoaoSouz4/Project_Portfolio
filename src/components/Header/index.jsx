9df3b22dfb2b4900c9517afd73664e2c984d5c14
João Souza <joaosouz8@gmail.com>
2023-03-04 20:26:31 -0300

    inital commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+export const Header = () => {
+    return(
+        <header>
+            <div>
+                MyLog
+            </div>
+
+            <nav>
+                <ul>
+                    <a>Home</a>
+                    <a>Quem sou</a>
+                    <a>Meus Projetos</a>
+                    <a>Contato</a>
+                </ul>
+            </nav>
+        </header>
+    )
+}
