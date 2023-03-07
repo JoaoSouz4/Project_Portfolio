0c4dfdb2ca00f2c00ff1dc53d2f569ee51b53736
João Souza <joaosouz8@gmail.com>
2023-03-07 19:58:04 -0300

    footer montado

--- src/components/Contato/index.jsx
+++ src/components/Contato/index.jsx
@@ -5,9 +5,10 @@ export const Contato = () => {
         <section className='section-apresentation'>
 
             <form action="" className='form-to-contact'>
-                <h1>Vamos trabalhar juntos</h1>
+                <h1>Vamos trabalhar <strong>juntos!</strong></h1>
                 <input type="email"  placeholder='Digite seu melhor email' id='input-email'/>
                 <textarea name="" id="input-text-area" cols="30" rows="10" placeholder='Mande me uma mensagem...'></textarea>
+                <button>Enviar</button>
             </form>
         </section>
     )
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,18 @@
 import './styles.css';
+import { AiOutlineInstagram, AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
 
 export const Footer = () => {
     return(
         <footer>
+
            <label>Desenvolvido por João Souza</label>
-           <label>0/00/0000</label>
+           <label>0/03/2023</label>
 
            <div className='my-social-midias'>
-                
+                <AiFillGithub className='icon-social-media'></AiFillGithub>
+                <AiFillLinkedin className='icon-social-media'></AiFillLinkedin>
+                <AiOutlineInstagram className='icon-social-media'></AiOutlineInstagram>
+                <AiOutlineMail className='icon-social-media'></AiOutlineMail>
            </div>
 
            <div className="my-location">
