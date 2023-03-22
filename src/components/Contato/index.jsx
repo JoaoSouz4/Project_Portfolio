6c81cca1f05bda95d110d120c51f8cb56e9f369e
João Souza <joaosouz8@gmail.com>
2023-03-21 23:33:53 -0300

    inicio do sistema de email

--- src/components/Contato/index.jsx
+++ src/components/Contato/index.jsx
@@ -1,16 +1,47 @@
 import './styles.css';
-import { Fade } from 'react-awesome-reveal'
+import { Fade } from 'react-awesome-reveal';
+import { useState } from 'react'
 
 export const Contato = () => {
-    return (
-        <section className='section-apresentation' id='contato'>
+    const [nameUser, setNameUser] = useState();
+    const [email, setEmail] = useState();
+    const [bodyEmail, setBodyEmail] = useState();
 
+    function sendEmail(e){
+        e.preventDefautl();
+        alert('envio teste')
+    }
+
+    return (
+        <section className='form section-contact' id='contato'>
             <Fade direction = "up">
-                <form action="" className='form-to-contact'>
+                <form className='form-to-contact'
+                >
                     <h1>Vamos trabalhar <strong>juntos!</strong></h1>
-                    <input type="email"  placeholder='Digite seu melhor email' id='input-email'/>
-                    <textarea name="" id="input-text-area" cols="30" rows="5" placeholder='Mande me uma mensagem...'></textarea>
-                    <button className='contact-button'>Enviar</button>
+
+                    <input type="text"
+                           placeholder='Digite seu nome' 
+                           id='input-name'
+                           onChange={(e)=>{setNameUser(e.target.value)}}
+                           value = {nameUser}
+                           />
+
+                    <input type="email"  
+                           placeholder='Digite seu melhor email'
+                           id='input-email'
+                           onChange={(e)=>{setEmail(e.target.value)}}
+                           value = {email}
+                           />
+
+                    <textarea name="" 
+                              id="input-text-area" 
+                              cols="30" rows="5" 
+                              placeholder='Mande me uma mensagem...'
+                              onChange={(e)=>{setBodyEmail(e.target.value)}}
+                              value = {bodyEmail}
+                            />
+                    
+                    <input type = "submit" className='contact-button' value='Enviar'/>
                 </form>
             </Fade>
         </section>
